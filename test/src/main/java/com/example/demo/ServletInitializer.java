fc4ca4038ba804367afeea8c52c998c9d5b04d27
mozuk <mozuk@Mot1>
2025-12-17 18:05:53 +0900

    aa

--- test/src/main/java/com/example/demo/ServletInitializer.java
+++ test/src/main/java/com/example/demo/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(TrainingspringApplication.class);
+		return application.sources(TestApplication.class);
 	}
 
 }
