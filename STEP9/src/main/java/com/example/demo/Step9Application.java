8678b7f9c7a186750acbbae0a8d25c13efc60edd
mozuk <mozuk@Mot1>
2025-12-18 17:48:10 +0900

    17:47 12/18

--- STEP9/src/main/java/com/example/demo/Step9Application.java
+++ STEP9/src/main/java/com/example/demo/Step9Application.java
@@ -1,8 +1,12 @@
 package com.example.demo;
 
+
+
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.example.demo.repository")
 @SpringBootApplication
 public class Step9Application {
 
